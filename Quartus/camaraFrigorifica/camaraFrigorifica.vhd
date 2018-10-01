6e427b133f7a70b91381179bec336cb7bdac8ea8
Mateus Paiva Matiazzi <201419050214@DOMINIO.FACOM.UFMS.BR>
2018-10-01 14:36:14 -0400

    Correção da questão 4 da lista 3

--- Quartus/camaraFrigorifica/camaraFrigorifica.vhd
+++ Quartus/camaraFrigorifica/camaraFrigorifica.vhd
@@ -15,23 +15,29 @@ end entity;
 architecture alg of camaraFrigorifica is
 begin
 	process(clk) is
-	variable aux: std_logic_vector(8 downto 0);
-	variable aux2: std_logic_vector(8 downto 0);
+	variable aux: natural;
+	variable aux2:natural;
 	variable count: natural;
 	begin
 		
 		if clk'event and clk = '1' then
 			if count = 0 then
-				aux:= aux + s1;
+				aux:= aux + to_integer(unsigned(s1));
+				count:=count + 1;
 			elsif count = 1 then
-				aux:= aux +s2;
+				aux:= aux +to_integer(unsigned(s2));
+				count:=count + 1;
 			elsif count = 2 then
-				aux:= aux + s3;
+				aux:= aux + to_integer(unsigned(s3));
+				count:=count + 1;
 			else
-				aux := aux + s4;
-				aux2:= std_logic_vector(shift_right(unsigned(aux), 2));
-				saida <= aux2(4 downto 0);
-				aux:="000000000";
+				aux := aux + to_integer(unsigned(s4));
+				if aux mod 2 = 1 then
+					aux := aux + 1;
+				end if;
+				aux2:= aux/4;
+				saida <=  std_logic_vector(to_unsigned(aux2, s1'length));
+				aux:=0;
 				count := 0;
 			end if;
 		end if;
